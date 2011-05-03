31becbb54430b491ab76cf61fab54eb3a794cffc
Larry Lindsey <llindsey@clm.utexas.edu>
2011-05-03 17:05:02 -0500

    Added argument to select the number of eigenvalues desired

--- doNCuts.m
+++ doNCuts.m
@@ -1,13 +1,17 @@
-function [e v D] = doNCuts(W)
-% [e v D] = doNCuts(W)
+function [e v D] = doNCuts(W, n)
+% [e v D] = doNCuts(W, n)
 % Performs an ncuts calculation on the graph-connection weight matrix W
 % 
 % For instance, to select the primary segmentation, do
 % D * v > 0
 % Use the column corresponding to the smallest nonzero eigenvalue to select
 % the segmentation groups.
+%
+% Returns the first n smalled eigenvalues, or 4 if n is unspecified.
 
-n = 4;
+if nargin < 2
+    n = 4;
+end
 
 if size(W,1) ~= size(W,2)
     error('W should be square');
@@ -21,6 +25,8 @@ Dinv = sparse(1:s, 1:s, 1./sum(W,1), s, s);
 
 
 M = sqrt(Dinv) * (D - W) * sqrt(Dinv);
+
+M = enforceSymmetric(M);
 
 [v d] = eigs(M, n, 'SM');
 e = d(logical(eye(n)));
