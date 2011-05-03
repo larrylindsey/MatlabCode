1846e4860a79eb683ce8438ef8302bfa377d8b25
Larry Lindsey <llindsey@clm.utexas.edu>
2011-05-03 17:08:43 -0500

    Major modifications, needed to return correspondence data

--- autosegmentation/selectLabelsByMask.m
+++ autosegmentation/selectLabelsByMask.m
@@ -14,75 +14,125 @@ if ~islogical(mask)
 end
 
 L = bwlabel(mask);
+n = max(L(:));
 
-labelMask = false(size(mask));
-tdata = [];
+labelMask = false([size(mask) n]);
+tdata = cell(1,n);
 
 for il = 1:max(L(:))
-    [labelMask tdata] = labelMaskHelper(label, L == il, lt, ut, labelMask,...
-        tdata);
-end
-
-end
-
-function [labelMask outdata] = labelMaskHelper(label, mask, lt, ut, ...
-    inLabelMask, indata)
+    subMask = L == il;
+    idL = double(unique(label(subMask)));
+    idL(idL == 0) = [];
+    innerScore = getInnerScore(label, subMask, idL);
+    innerSelect = innerScore >= lt;
     
-lid = unique(label(mask));
-lid(lid == 0) = [];
-
-% If a segment is approximately internal to the selection mask, add it to
-% the output.
-
-innerSelect = false(size(lid));
-labelMask = false(size(mask));
-innerScore = zeros(size(lid));
-
-for ii = 1:numel(lid)
-    currLabelMask = label == lid(ii);
-    innerScore(ii) = directionalCover(currLabelMask, mask);
-    if innerScore(ii) >= lt
-        innerSelect(ii) = true;
-        labelMask = or(labelMask, currLabelMask);
+    outterScore = getOutterScore(label, subMask, idL, innerSelect);
+    outterSelect = outterScore >= ut;
+    
+    tdata{il} = cat(2, idL(:), innerScore(:), innerSelect(:),...
+        outterScore(:), outterSelect(:));
+    
+    labelSelect = cat(1, idL(innerSelect(:)), idL(outterSelect(:)));
+    
+    for ii = 1:numel(labelSelect)
+        labelMask(:,:,il) = ...
+            or(labelMask(:,:,il), label == labelSelect(ii));
     end
+    
+%     [labelMask tdata] = labelMaskHelper(label, L == il, lt, ut, labelMask,...
+%         tdata);
 end
 
-innerLid = lid(innerSelect);
-innerScore = innerScore(innerSelect);
-innerLid = innerLid(:);
-innerScore = innerScore(:);
-lid = lid(not(innerSelect));
+labelMask = max(labelMask, [], 3);
+tdata = cat(1, tdata{:});
 
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function score = getInnerScore(label, mask, idL)
+score = zeros(numel(idL), 1);
+parfor ii = 1:numel(idL)
+    currLabelMask = label == idL(ii);
+    score(ii) = directionalCover(currLabelMask, mask);
+end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function outScore = getOutterScore(label, mask, idL, exclude)
 
-outterSelect = false(size(lid));
-outterScore = zeros(size(lid));
-outterMask = mask;
-% Don't consider the contributions to the inner selections for this step.
-outterMask(labelMask) = false;
+outScore = zeros(numel(idL), 1);
+idLi = idL(not(exclude));
+idLe = idL(exclude);
 
-for ii = 1:numel(lid)
-    currLabelMask = label == lid(ii);
-    outterScore(ii) = nonDirectionalCover(currLabelMask, outterMask);
-    if outterScore(ii) >= ut
-        outterSelect(ii) = true;
-        labelMask = or(labelMask, currLabelMask);
-    end
+score = zeros(numel(idLi), 1);
+
+for ii = 1:numel(idLe)
+    mask(label == idLe(ii)) = false;
 end
 
-outterLid = lid(outterSelect);
-outterScore = outterScore(outterSelect);
-outterLid = outterLid(:);
-outterScore = outterScore(:);
-
-labelMask = or(labelMask, inLabelMask);
-lid = cat(1, innerLid, outterLid);
-scores = cat(1, innerScore, outterScore);
-indices = cat(1, zeros(numel(innerScore), 1), ones(numel(outterScore), 1));
-
-
-outdata = cat(1, indata, cat(2, lid, scores, indices));
+parfor ii = 1:numel(idLi)
+    currLabelMask = label == idLi(ii);
+    score(ii) = nonDirectionalCover(currLabelMask, mask);
 end
 
+outScore(not(exclude)) = score;
 
-
-
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function [labelMask outdata] = labelMaskHelper(label, mask, lt, ut, ...
+%     inLabelMask, indata)
+%     
+% lid = unique(label(mask));
+% lid(lid == 0) = [];
+% 
+% % If a segment is approximately internal to the selection mask, add it to
+% % the output.
+% 
+% innerSelect = false(size(lid));
+% labelMask = false(size(mask));
+% innerScore = zeros(size(lid));
+% 
+% for ii = 1:numel(lid)
+%     currLabelMask = label == lid(ii);
+%     innerScore(ii) = directionalCover(currLabelMask, mask);
+%     if innerScore(ii) >= lt
+%         innerSelect(ii) = true;
+%         labelMask = or(labelMask, currLabelMask);
+%     end
+% end
+% 
+% innerLid = lid(innerSelect);
+% innerScore = innerScore(innerSelect);
+% innerLid = innerLid(:);
+% innerScore = innerScore(:);
+% 
+% 
+% outterSelect = false(size(lid));
+% outterScore = zeros(size(lid));
+% outterMask = mask;
+% % Don't consider the contributions to the inner selections for this step.
+% outterMask(labelMask) = false;
+% 
+% seq = 1:numel(lid);
+% seq(innerSelect) = [];
+% 
+% for ii = seq
+%     currLabelMask = label == lid(ii);
+%     outterScore(ii) = nonDirectionalCover(currLabelMask, outterMask);
+%     if outterScore(ii) >= ut
+%         outterSelect(ii) = true;
+%         labelMask = or(labelMask, currLabelMask);
+%     end
+% end
+% 
+% outterLid = lid(outterSelect);
+% outterScore = outterScore(outterSelect);
+% outterLid = outterLid(:);
+% outterScore = outterScore(:);
+% 
+% labelMask = or(labelMask, inLabelMask);
+% lid = cat(1, innerLid, outterLid);
+% scores = cat(1, innerScore, outterScore);
+% indices = cat(1, zeros(numel(innerScore), 1), ones(numel(outterScore), 1));
+% 
+% 
+% outdata = cat(1, indata, cat(2, lid, scores, indices));
+% end
