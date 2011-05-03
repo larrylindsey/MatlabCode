1d731ed8ef2cb246190c8ca0611dbd41a6cd6228
Larry Lindsey <llindsey@clm.utexas.edu>
2011-05-03 17:06:29 -0500

    Pretty major rewrites

--- autosegmentation/autoSegmentReconstruct.m
+++ autosegmentation/autoSegmentReconstruct.m
@@ -1,76 +1,179 @@
-function stackOut = autoSegmentReconstruct(secdoc, cname, index, control)
+function [stackOut matchData score] = autoSegmentReconstruct(secdoc, ...
+    cname, index, cstr)
 
 if nargin < 4
-    control = defaultControl;
+    cstr = defaultControl;
     if nargin == 0
-        stackOut = control;
+        stackOut = cstr;
         return;
     else
-        control.cstr.imsz = [];
         [x y] = reconstructDomainBounds(secdoc);
-        control.cstr.x = x;
-        control.cstr.y = y;
-        control.cstr.eT = .5;
-        control.cstr.iT = .8;
+        cstr.x = x;
+        cstr.y = y;
+        cstr.eT = .5;
+        cstr.iT = .8;
+        cstr.scoreT = .75;
     end
 end
 
 
-if isempty(control.classImage)
-    classImFun = @defaultClassImFun;
-else
-    classImFun = control.classImage;
-end
-
-if isempty(control.classThreshold)
-    classTh = @defaultClassThreshold;
-else
-    classTh = control.classThreshold;
-end
-
-cstr = control.cstr;
-
-
 % Get first match mask
 annoMask = getRawImageContourSlice(secdoc, index, cname);
-classOrig = classImFun(secdoc, index);
-labelOrig = fijiTrainableSegmentationAnnotation(classTh(classOrig));
 trOrig = secdocTrans(secdoc, index);
-maskOrig = nextLabelMask(annoMask, labelOrig, trOrig, trOrig, cstr);
+labelOrig = readLabelIm(index);
+cstr.imsz = size(labelOrig);
 
-%stackOut = maskOrig;
-stackOut = [];
+annoMaskTR = applyTransformImage(annoMask, trOrig, x, y);
+labelOrig = sizeImage(labelOrig, size(annoMaskTR));
+
+
+[maskOrig dataOrig] = ...
+    selectLabelsByMask(labelOrig, annoMaskTR, cstr.iT, cstr.eT);
+
+
+stackOut = false(0);
+matchData = {};
+score = [];
 nextMask = maskOrig;
+nextData = dataOrig;
+nextScore = -1;
 
-%cstr.imsz = size(maskOrig);
+doRepartition = false;
 
-while any(nextMask(:))
-    stackOut = cat(3, stackOut, nextMask);
-    currMask = nextMask;
-    index = index + 1;
-    fprintf('Now segmenting index %d\n', index);
-    class = classImFun(secdoc, index);
-    label = fijiTrainableSegmentationAnnotation(classTh(class));
-    tr = secdocTrans(secdoc, index);
-    nextMask = nextLabelMask(currMask, label, [], tr, cstr);    
+
+while any(nextMask(:)) || doRepartition
+    
+    if ~doRepartition
+        
+        stackOut = cat(3, stackOut, nextMask);
+        matchData = cat(2, matchData, nextData);
+        score = cat(2, score, nextScore);
+        currMask = nextMask;
+        
+        index = index - 1;
+        
+        fprintf('Now segmenting index %d\n', index);
+        
+        label = readLabelIm(index);
+        label = sizeImage(label, size(currMask));    
+
+    else
+        fprintf('Re segmenting index %d\n', index);
+    end
+
+    
+    [nextMask nextData] = selectLabelsByMask(label, currMask, cstr.iT,...
+        cstr.eT);
+    
+    nextScore = nonDirectionalCover(nextMask, currMask);
+    
+    if nextScore < cstr.scoreT        
+        return;
+        if doRepartition
+            fprintf('Repartitioning %d led to no further segmentation\n',...
+                index);
+            return;
+        end
+        disp('Score did not meet requirement. Resegmenting');
+        doRepartition = true;
+        
+        label = nCutPartition(label, currMask, nextData, index);
+    else
+        doRepartition = false;
+    end
 end
+
+stackOut = cat(3, stackOut, nextMask);
+matchData = cat(2, matchData, nextData);
+score = cat(2, score, nextScore);
 
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function classIm = defaultClassImFun(section, index)
-t = secdocTrans(section, index);
-fname = ['class_' t.Image.src];
-classIm = im2double(imread(fname));
+function partLabel = nCutPartition(label, mask, data, index)
+partLabel = label;
+% This usually should happen because the mask is much smaller than an
+% under-segmented component in the label that it would otherwise correspond
+% to.
+% Here, find that segment by figuring out which component the mask fits
+% best within.
+
+n = 4;
+
+idL = data(:,1);
+matchScore = zeros(size(idL));
+
+parfor il = 1:numel(idL)
+    matchScore(il) = directionalCover(mask, label == idL(il));
+end
+
+[junk iMatch] = max(matchScore);
+
+segMask = label == idL(iMatch);
+
+classIm = readClassIm(index);
+classIm = sizeImage(classIm, size(segMask));
+
+[W junk cropMask offset] = nCutMaskedImage(segMask, classIm, 4, [1 1]);
+offset = offset - 1;
+
+[e v d] = doNCuts(W, n);
+
+segValue = zeros([size(cropMask) n]);
+
+for ii = 1:n
+    eSeg = zeros(size(cropMask));
+    eSeg(cropMask) = d * v(:,ii);
+    segValue(:,:,ii) = eSeg;
+end
+
+%segValue = [];
+
+segSign = prod(sign(segValue(:,:,e ~= min(e))), 3);
+
+posSeg = segSign > 0;
+negSeg = segSign < 0;
+
+posSeg = imerode(posSeg, strel('disk', 3));
+negSeg = imerode(negSeg, strel('disk', 3));
+
+posLabel = bwlabel(posSeg, 4);
+negLabel = bwlabel(negSeg, 4);
+
+negLabel(negLabel > 0) = negLabel(negLabel > 0) + max(posLabel(:));
+
+newLabel = negLabel + posLabel;
+newLabel = imdilate(newLabel, strel('disk', 1));
+
+repMask = false(size(label));
+repMask(offset(1) + (1:size(cropMask,1)), offset(2) + (1:size(cropMask,2))) ...
+    = cropMask;
+
+partLabel(repMask) = newLabel(cropMask);
+partLabel(repMask) = partLabel(repMask) + max(partLabel(:));
+
+partLabel(partLabel == max(partLabel(:))) = idL(iMatch);
+
+
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function bw = defaultClassThreshold(imclass)
-bw = imclass > .75;
+function classIm = readClassIm(index)
+classIm = im2double(readCacheIm('class', index));
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function control = defaultControl
-control.classImage = @defaultClassImFun;
-control.classThreshold = @defaultClassThreshold;
-control.cstr = nextLabelMask;
+function labelIm = readLabelIm(index)
+labelIm = readCacheIm('label', index);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function cacheIm = readCacheIm(prefix, index)
+cacheIm = imread(sprintf('seg_cache/%s_%04d.png', prefix, index));
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function cstr = defaultControl
+cstr.x = [];
+cstr.y = [];
+cstr.eT = .5;
+cstr.iT = .8;
+cstr.scoreT = .75;
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function t = secdocTrans(secdoc, index)
