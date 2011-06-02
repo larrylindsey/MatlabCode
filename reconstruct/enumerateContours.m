cc0c45d23c253b9414091a174dd05cdde7ebd130
Larry Lindsey <llindsey@clm.utexas.edu>
2011-06-02 13:02:44 -0500

    Now, with parallel support

--- reconstruct/enumerateContours.m
+++ reconstruct/enumerateContours.m
@@ -1,20 +1,32 @@
 function names = enumerateContours(secdoc)
 
-names = {};
+p = matlabpool('size');
 
+secdocSplit = cell(1, p);
 
-for i_sec = 1:numel(secdoc)
-    transforms = secdoc(i_sec).section.Transform;
-    for i_t = 1:numel(transforms)
-        tform = transforms(i_t);
-        for i_c = 1:numel(tform.Contour)
-            catName = tform.Contour(i_c).name;
-            if ~ischar(catName)
-                catName = num2str(catName);
-            end
-            names = unique({names{:} catName});
-        end
-    end    
+for ii = 1:p
+    secdocSplit{ii} = secdoc(ii:p:end);
 end
 
+names = cell(1,p);
+
+parfor ip = 1:p
+    names{ip} = {};
+    for i_sec = 1:numel(secdocSplit{ip})
+        transforms = secdocSplit{ip}(i_sec).section.Transform;
+        for i_t = 1:numel(transforms)
+            tform = transforms(i_t);
+            for i_c = 1:numel(tform.Contour)
+                catName = tform.Contour(i_c).name;
+                if ~ischar(catName)
+                    catName = num2str(catName);
+                end
+                names{ip} = cat(1, names{ip}, catName);
+            end
+        end
+    end
+    %names{ip} = unique(names{ip});
+end
+
+names = unique(cat(1, names{:}));
 end
