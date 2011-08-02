497b7502b66e20caa51b1bc3a788c4363a30c837
Larry Lindsey <llindsey@clm.utexas.edu>
2011-08-02 17:43:28 -0500

    Automatically add third party functions to the path

---
+++ thirdpartyfunction.m
@@ -0,0 +1,25 @@
+function varargout = thirdpartyfunction(func, varargin)
+
+codedir = '/home/larry/code/3rd-party-matlab';
+
+if ischar(func)
+    func = str2func(func);
+end
+
+funcid = functions(func);
+if isempty(funcid.file)
+    cmd = sprintf(['find %s/ -name %s.* '...
+        '| sed -e "%s" | gawk ''{ print $5 }'''], ...
+        codedir, funcid.function, 's/\// /g');
+    [junk fdir] = unix(cmd); %#ok
+    fdir = textscan(fdir, '%s');
+    adir = fdir{1};
+    
+    for ii = 1:numel(adir)
+        addpath(genpath([codedir '/' adir{ii}]));
+    end
+end
+
+varargout = cell(1, nargout);
+
+[varargout{:}] = func(varargin{:});
