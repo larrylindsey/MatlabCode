8c9bdbd180eb569566a022b4c6b204534b994960
Larry Lindsey <llindsey@clm.utexas.edu>
2011-10-31 17:17:20 -0500

    Now does mag and orientation maps

--- gridDistortion/makeDistortionMap.m
+++ gridDistortion/makeDistortionMap.m
@@ -1,35 +1,16 @@
-function [dmap] = makeDistortionMap(trans, n, lim)
+function [dmag dcolor RC RCtr] = makeDistortionMap(trans, n, lim)
 
 if nargin < 3
-    lim = [0 1];
+    lim = [-1 1];
 end
-% 
-% [R C] = meshgrid(linspace(lim(1), lim(2), 16), linspace(lim(1), lim(2), 16));
-% RC = cat(2, R(:), C(:));
-% RCtr = doTransform(RC, trans);
-% 
-% % ATrans.order = 1;
-% % ATrans.T = calculateTransMat(RCtr, RC, ...
-% %     ATrans.order, type);
-% % ATrans.Tinv = calculateTransMat(RC, RCtr, ...
-% %     ATrans.order, type);
-% % ATrans.type = type;
-% 
-% 
-% 
-% %RCtr = doTransform(RCtr, ATrans);
-% [RCtrLA sh r] = jankyLinearAlign(RCtr, RC, .1, .1);
+
 
 [R C] = meshgrid(linspace(lim(1), lim(2), n), linspace(lim(1), lim(2), n));
-RC = cat(2, R(:), C(:));
+RC = cat(2, C(:), R(:));
 RCtr = doTransform(RC, trans);
 
-% RCtr = jankyLinearAlign(RCtr, RC);
-% RCtr = RCtr + repmat(sh, [size(RCtr,1), 1]);
-% zs = repmat(mean(RC,1), [size(RC,1) 1]);
 
-% RCtr = RCtr - zs;
-% RCtr = RCtr * r;
-% RCtr = RCtr + zs;
+dmag = reshape(sqrt(sum((RC - RCtr).^2, 2)), size(R));
 
-dmap = reshape(sqrt(sum((RC - RCtr).^2, 2)), size(R));
+dcolor = vectorMapToImage(RC(:,2) - RCtr(:,2), RC(:,1) - RCtr(:,1));
+dcolor = reshape(dcolor, [n n 3]);
