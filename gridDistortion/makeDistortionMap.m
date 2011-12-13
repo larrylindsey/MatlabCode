6080b94ef1b4b5b29c63eabd64dd5ae27b6c3c70
Larry Lindsey <llindsey@clm.utexas.edu>
2011-12-13 15:47:17 -0600

    Makes a distortion map figure

--- gridDistortion/makeDistortionMap.m
+++ gridDistortion/makeDistortionMap.m
@@ -1,16 +1,8 @@
-function [dmag dcolor RC RCtr] = makeDistortionMap(trans, n, lim)
-
-if nargin < 3
-    lim = [-1 1];
-end
+function [dmag dcolor] = makeDistortionMap(RC, RCtr, t, sz)
 
 
-[R C] = meshgrid(linspace(lim(1), lim(2), n), linspace(lim(1), lim(2), n));
-RC = cat(2, C(:), R(:));
-RCtr = doTransform(RC, trans);
 
-
-dmag = reshape(sqrt(sum((RC - RCtr).^2, 2)), size(R));
-
-dcolor = vectorMapToImage(RC(:,2) - RCtr(:,2), RC(:,1) - RCtr(:,1));
-dcolor = reshape(dcolor, [n n 3]);
+[dcolor dmag] = vectorMapToImage(RC(:,2) - RCtr(:,2), RC(:,1) - RCtr(:,1), ...
+    t);
+dcolor = reshape(dcolor, [sz(1) sz(2) 3]);
+dmag = reshape(dmag, sz);
