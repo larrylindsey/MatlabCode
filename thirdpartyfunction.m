f40ffdeef6d1349f67871318c52a8e4123b2a678
Larry Lindsey <llindsey@clm.utexas.edu>
2011-12-13 15:49:44 -0600

    Added help message, use awk instead of gawk

--- thirdpartyfunction.m
+++ thirdpartyfunction.m
@@ -1,4 +1,17 @@
 function varargout = thirdpartyfunction(func, varargin)
+% <[argout1 argout2 ... argoutM]> =
+%                      thirdpartyfunction(func, <argin1 argin2 ... arginN>)
+%  
+%  Finds and executes the given function, when it is not part of the path,
+%   but exists in a pre-defined directory.
+%
+% func - the function to execute. May be either a string or a function
+%        handle
+%
+%
+% This function requires the 'awk' executable to be present and in
+% Matlab's execution path.
+
 
 codedir = '/home/larry/code/3rd-party-matlab';
 
@@ -9,7 +22,7 @@ end
 funcid = functions(func);
 if isempty(funcid.file)
     cmd = sprintf(['find %s/ -name %s.* '...
-        '| sed -e "%s" | gawk ''{ print $5 }'''], ...
+        '| sed -e "%s" | awk ''{ print $5 }'''], ...
         codedir, funcid.function, 's/\// /g');
     [status fdir] = unix(cmd);
     
