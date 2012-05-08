667e2d1a7012933d1a79c7df42d279b35038bfe0
Larry Lindsey <lllindsey@clm.utexas.edu>
2012-05-08 18:57:17 -0500

    Affine inverse didn't take legendre into account. Fixed it

--- gridDistortion/populateTransInverse.m
+++ gridDistortion/populateTransInverse.m
@@ -8,7 +8,7 @@ if isempty(trans.T)
 end
 
 T = trans.T;
-if any(T(4:end,:) ~= 0)
+% if any(T(4:end,:) ~= 0)
     if isfield(trans, 'data')
         if isfield(trans.data, 'u')
             u = trans.data.u;
@@ -32,15 +32,15 @@ if any(T(4:end,:) ~= 0)
     end
     support = [min([u(:) ; v(:)]) max([u(:) ; v(:)])];
     Tinv = invertTransform(T, trans.type, n, support);
-else
-    A = T(2:3,:)';
-    Ainv = inv(A);
-    c = T(1,:)';
-    
-    cinv = - Ainv * c;
-    
-    Tinv = cat(1, cinv', Ainv', zeros(size(T, 1) - 3, 2));
-    
-end
+% else
+%     A = T(2:3,:)';
+%     Ainv = inv(A);
+%     c = T(1,:)';
+%     
+%     cinv = - Ainv * c;
+%     
+%     Tinv = cat(1, cinv', Ainv', zeros(size(T, 1) - 3, 2));
+%     
+% end
 
 trans.Tinv = Tinv;
