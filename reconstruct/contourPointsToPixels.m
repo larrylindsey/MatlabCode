ed2ea471db1160e527c0164f08f47f47d9b76e3f
Larry Lindsey <llindsey@clm.utexas.edu>
2012-07-08 18:32:56 -0500

    Adds pixelPts to contour structs

---
+++ reconstruct/contourPointsToPixels.m
@@ -0,0 +1,37 @@
+function contours = contourPointsToPixels(secdoc, contours)
+
+if nargin < 2
+    contours = enumerateContours(secdoc);
+end
+
+if isstruct(contours)
+    contours = {contours};
+end
+
+if ischar(contours{1})
+    for ii = 1:numel(contours)
+        contours{ii} = extractContour(secdoc, contours{ii});
+    end
+end
+
+secIndices = [secdoc.index];
+
+for ii = 1:numel(contours)
+    contour = contours{ii};
+    for jj = 1:numel(contour)
+        sec = secdoc(secIndices == contour(jj).section).section;
+        tr = sec.Transform(sec.transImageIndex);
+        
+        tpts = contour(jj).transPoints;
+        tpts = applyInverseTransform(tpts, tr);
+        tpts = tpts / sec.Transform(sec.transImageIndex).Image.mag;
+        contour(jj).pixelPts = tpts;
+        
+        contour(jj).imsz = ...
+            max(sec.Transform(sec.transImageIndex).Contour(1).points, [], 1);
+        contour(jj).mag = sec.Transform(sec.transImageIndex).Image.mag;
+    end
+    contours{ii} = contour;
+end
+
+end
