20277cd8d00f516d41f34068b1b7572ab6f49557
Larry Lindsey <lllindsey@clm.utexas.edu>
2012-07-18 11:09:12 -0500

    Fixed for empty case

--- catstruct.m
+++ catstruct.m
@@ -1,8 +1,13 @@
 function sout = catstruct(s1, s2)
-sout = s1;
-sout(end + 1) = s1(end);
-ff = fieldnames(sout);
-
-for ii = 1:numel(ff)
-   sout(end).(ff{ii}) = s2.(ff{ii});
+if numel(s1) < 1
+    sout = s2;
+else
+    
+    sout = s1;
+    sout(end + 1) = s1(end);
+    ff = fieldnames(sout);
+    
+    for ii = 1:numel(ff)
+        sout(end).(ff{ii}) = s2.(ff{ii});
+    end
 end
