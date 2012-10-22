c8074cbcfffdb764ce58fde1b3c502190769d377
Larry Lindsey <llindsey@clm.utexas.edu>
2012-10-22 12:13:01 -0500

    Simplified plot generation code

--- reconstruct/doSfNPlots.m
+++ reconstruct/doSfNPlots.m
@@ -1,8 +1,4 @@
-function doSfNPlots(k24, oblique, volumejosef)
-
-figure;
-firstsub();
-ax = gca;
+function doSfNPlots(k24, oblique, volumejosef, jstats)
 
 
 % Plot options
@@ -16,7 +12,8 @@ cmap = cat(2, c_k24', c_obl', c_vol');
 
 
 opt_k24 = makeVolCompPlots();
-opt_k24.axis = ax;
+opt_k24.axis = 0;
+opt_k24.font = 'Arial';
 
 opt_k24.fontsize = labelsize;
 opt_k24.color = c_k24;
@@ -28,7 +25,7 @@ opt_vol = opt_k24;
 opt_vol.color = c_vol;
 
 legnames = {'Spine', 'Oblique', 'Apical'};
-xticks = -.5:0.05:.5;
+xticks = -.5:0.1:.5;
 yticks = 0:.1:1;
 
 allax = [];
@@ -40,107 +37,80 @@ binctr = linspace(-.5, .5, 50);
 % hold on;
 % plot([-.1 -.1 nan .1 .1], [0 1 nan 0 1], 'k', 'LineWidth', 2);
 
-stat_k24 = makeVolCompPlots(k24, 'dendriteOrig', 'dendriteElastic', 'Volume',...
-    opt_k24);
-stat_obl = makeVolCompPlots(oblique, 'dendriteAffine', 'dendriteElastic',...
-    'Volume', opt_obl);
-stat_vol = makeVolCompPlots(volumejosef, 'dendriteAffine', 'dendriteElastic',...
-    'Volume', opt_vol);
-xlim([-.5 .5]);
+objtype = {'dendrite', 'axon', 'cfa'};
+titles = {'Dendrite Volume', 'Axon Volume', 'Synapse Area'};
+mtype = {'Volume', 'Volume', 'Flatarea'};
 
-title(sprintf('Percent Change in Dendrite Volume\nCumulative Distribution'),...
-    'FontSize', titlesize);
-lh = legend(legnames{:}, 'Location', 'SouthEast');
-set(lh, 'FontSize', 12);
+table_label = {'n', '+/- 5%', '+/- 10%', 'mean'};
 
-h_k24 = hist(stat_k24, binctr);
-h_obl = hist(stat_obl, binctr);
-h_vol = hist(stat_vol, binctr);
-h_all = cat(1, h_k24, h_obl, h_vol)';
+table_stat = zeros(4, 4, numel(objtype));
 
-allax = [allax gca];
+for ii = 1:numel(objtype)
+    figure;
+    firstsub();
+    ax = gca;
+    opt_k24.axis = ax; opt_obl.axis = ax; opt_vol.axis = ax;
+    
+    
+    stat_k24 = makeVolCompPlots(k24, [objtype{ii} 'Orig'], ...
+        [objtype{ii} 'Elastic'], mtype{ii},...
+        opt_k24);
+    stat_obl = makeVolCompPlots(oblique, [objtype{ii} 'Orig'], ...
+        [objtype{ii} 'Elastic'], mtype{ii},...
+        opt_obl);
+    stat_vol = makeVolCompPlots(volumejosef, [objtype{ii} 'Orig'], ...
+        [objtype{ii} 'Elastic'], mtype{ii},...
+        opt_vol);
+    
+    xlim([-.5 .5]);
+    
+    title(sprintf('Percent Change in %s\nCumulative Distribution', ...
+        titles{ii}), 'FontSize', titlesize);
+    lh = legend(legnames{:}, 'Location', 'SouthEast');
+    set(lh, 'FontSize', 12);
+    
+    h_k24 = hist(stat_k24, binctr);
+    h_obl = hist(stat_obl, binctr);
+    h_vol = hist(stat_vol, binctr);
+    h_all = cat(1, h_k24, h_obl, h_vol)';
+    
+    allax = [allax gca];
+    
+    secondsub();
+    bar(binctr, h_all, 1, 'stacked');
+    set(gca, 'FontSize', labelsize);
+    title('Histogram', 'FontSize', titlesize);
+    colormap(cmap);
+    xlim([-.5 .5]);
+    allax = [allax gca];
+    
+    table_stat(2:4, 1, ii) = [numel(stat_vol) numel(stat_obl) numel(stat_k24)];
+    table_stat(2:4, 2, ii) = [ sum(abs(stat_vol) < .05), sum(abs(stat_obl) < .05),...
+            sum(abs(stat_k24) < .05)];
+    table_stat(2:4, 3, ii) = [ sum(abs(stat_vol) < .1), sum(abs(stat_obl) < .1),...
+            sum(abs(stat_k24) < .1)];
+    table_stat(2:4, 4, ii) = [ mean(stat_vol), mean(stat_obl), mean(stat_k24) ];
+    
+    table_stat(1, 1:3, ii) = sum(table_stat(2:4, 1:3, ii), 1);
+    table_stat(1, 4, ii) = mean(cat(2, stat_vol, stat_obl, stat_k24));
+    
+    
+end
 
-secondsub();
-bar(binctr, h_all, 1, 'stacked');
-set(gca, 'FontSize', labelsize);
-title('Histogram', 'FontSize', titlesize);
-colormap(cmap);
-xlim([-.5 .5]);
-allax = [allax gca];
+table_stat(:, 2, :) = table_stat(:, 2, :) ./ table_stat(:, 1, :);
+table_stat(:, 3, :) = table_stat(:, 3, :) ./ table_stat(:, 1, :);
 
-% Axons
-figure;
-firstsub();
-ax = gca;
+fprintf('Table output, Volumes\n');
 
-% hold on;
-% plot([-.1 -.1 nan .1 .1], [0 1 nan 0 1], 'k', 'LineWidth', 2);
-
-opt_k24.axis = ax; opt_obl.axis = ax; opt_vol.axis = ax;
-
-stat_k24 = makeVolCompPlots(k24, 'axonOrig', 'axonElastic', 'Volume',...
-    opt_k24);
-stat_obl = makeVolCompPlots(oblique, 'axonAffine', 'axonElastic',...
-    'Volume', opt_obl);
-stat_vol = makeVolCompPlots(volumejosef, 'axonAffine', 'axonElastic',...
-    'Volume', opt_vol);
-xlim([-.5 .5]);
-title(sprintf('Percent Change in Axon Volume\nCumulative Distribution'),...
-    'FontSize', titlesize);
-
-lh = legend(legnames{:}, 'Location', 'SouthEast');
-set(lh, 'FontSize', 12);
-
-h_k24 = hist(stat_k24, binctr);
-h_obl = hist(stat_obl, binctr);
-h_vol = hist(stat_vol, binctr);
-h_all = cat(1, h_k24, h_obl, h_vol)';
-allax = [allax gca];
-
-secondsub();
-bar(binctr, h_all, 1, 'stacked');
-set(gca, 'FontSize', labelsize);
-title('Histogram', 'FontSize', titlesize);
-colormap(cmap);
-xlim([-.5 .5]);
-allax = [allax gca];
-
-% CFAs
-figure;
-firstsub();
-ax = gca;
-
-% hold on;
-% plot([-.1 -.1 nan .1 .1], [0 1 nan 0 1], 'k', 'LineWidth', 2);
-
-opt_k24.axis = ax; opt_obl.axis = ax; opt_vol.axis = ax;
-
-stat_k24 = makeVolCompPlots(k24, 'cfaOrig', 'cfaElastic', 'Flatarea',...
-    opt_k24);
-stat_obl = makeVolCompPlots(oblique, 'cfaAffine', 'cfaElastic',...
-    'Flatarea', opt_obl);
-stat_vol = makeVolCompPlots(volumejosef, 'cfaAffine', 'cfaElastic',...
-    'Flatarea', opt_vol);
-xlim([-.5 .5]);
-title(sprintf('Percent Change in Contact Flat Area\nCumulative Distribution'),...
-    'FontSize', titlesize);
-
-lh = legend(legnames{:}, 'Location', 'SouthEast');
-set(lh, 'FontSize', 12);
-
-h_k24 = hist(stat_k24, binctr);
-h_obl = hist(stat_obl, binctr);
-h_vol = hist(stat_vol, binctr);
-h_all = cat(1, h_k24, h_obl, h_vol)';
-allax = [allax gca];
-
-secondsub();
-bar(binctr, h_all, 1, 'stacked');
-set(gca, 'FontSize', labelsize);
-title('Histogram', 'FontSize', titlesize);
-colormap(cmap);
-xlim([-.5 .5]);
-allax = [allax gca];
+for ii = 1:size(table_stat,3)
+    fprintf('%s,All, Apical, Oblique, Spine\n', titles{ii});
+    for jj = 1:numel(table_label)
+        fprintf('%s, %g, %g, %g, %g\n', table_label{jj}, ...
+            table_stat(1, jj, ii), table_stat(2, jj, ii), ...
+            table_stat(3, jj, ii), table_stat(4, jj, ii));
+    end
+    fprintf('\n');
+end
 
 
 set(allax, 'XTickMode', 'manual');
@@ -148,10 +118,13 @@ set(allax, 'XTick', xticks);
 set(allax(1:2:end), 'YTickMode', 'manual');
 set(allax(1:2:end), 'YTick', yticks);
 
+
+
+
 end
 
 function firstsub
-subplot(11, 1, [2:5]);
+subplot(11, 1, 2:5);
 end
 
 function secondsub
