a9d78b2763c5752c5e9928da8ce5f768bc333827
Larry Lindsey <llindsey@clm.utexas.edu>
2012-10-24 18:46:22 -0500

    Cleaning up unused or temp code

--- fwdReconstructTransform.m
+++
@@ -1,8 +0,0 @@
-function [x y] = fwdReconstructTransform(xin, yin)
-
-a = [-2.54387 1.12134 0.175902 -0.000795219];
-b = [1.28503 -0.113561 0.964423 0.000795219];
-
-x = a(1) + a(2) * xin + a(3) * yin + a(4) * xin .* yin;
-y = b(1) + b(2) * xin + b(3) * yin + b(4) * xin .* yin;
-
