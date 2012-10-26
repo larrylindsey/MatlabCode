9053bf064428c644c58084996778a013c0218eca
Larry Lindsey <llindsey@clm.utexas.edu>
2012-10-26 11:48:11 -0500

    Cleaning up EM distortion code

--- gridDistortion/transformToTR.m
+++
@@ -1,9 +0,0 @@
-function tr = transformToTR(trans)
-
-if isempty(trans.Tinv)
-    trans.Tinv = invertTransform(trans.T, type, trans.data.n, ...
-        trans.data.u);
-end
-
-tr = maketform('custom', trans.iDim, trans.oDim, @doCubicTransform, ...
-    @doInverseCubicTransform, trans);
