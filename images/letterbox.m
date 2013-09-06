ce25715d5750df68ed0764aa414ce0d057fde8ca
Larry Lindsey <llindsey@clm.utexas.edu>
2013-09-06 11:09:26 -0500

    letterbox: passing class into zeros doesn't always work, cast instead

--- images/letterbox.m
+++ images/letterbox.m
@@ -3,9 +3,9 @@ function imout = letterbox(im, sz)
 sz = sz(1:2);
 sz(3) = size(im, 3);
 
-% castfunc = str2func(class(im));
-% 
-% imout = castfunc(zeros(sz));
+castfunc = str2func(class(im));
+
+imout = castfunc(zeros(sz));
 
 if size(im, 1) > sz(1)
     im = imresize(im, sz(1) / size(im,1));
@@ -16,7 +16,7 @@ if size(im, 2) > sz(2)
 end
 
 
-imout = zeros(sz, class(im));
+% imout = zeros(sz, class(im));
 
 rows = size(im, 1);
 cols = size(im, 2);
