e3052ab8ed3197caaad3eacbe0f222a9d91bce24
Larry Lindsey <llindsey@clm.utexas.edu>
2014-06-16 12:56:38 -0500

    imagesegmenstats: updated per Michelle via dropbox

--- images/imagesegmentstats.m
+++ images/imagesegmentstats.m
@@ -12,38 +12,38 @@ function [stat_str, masks] = imagesegmentstats(imagefile, outputfile)
 
 % Predefined colors
 im_kc = [255     0     0;
-    184    0     28;
-    26    93     0;
-    43    93     0;
-   255   255     0;
-     0     0   255;
-   137     0   255;
-   96     31   151;
-   169     0   185;
-     0   255   255;
-   255   153     0;
-     0     0     0] / 255;
+         184    0     28;
+         26    93     0;
+         43    93     0;
+         255   255     0;
+         0     0   255;
+         146     6   181;
+         96     31   151;
+         255     0   243;
+         0   255   255;
+         255   153     0;
+         0     0     0] / 255;
 
 if isempty(imagefile)
-  [fname, pname] = uigetfile();
-  
-  if pname(end) ~= '/'
-    pname = [pname '/'];
-  end
-  imagefile = [pname fname];
+[fname, pname] = uigetfile();
+
+if pname(end) ~= '/'
+pname = [pname '/'];
+end
+imagefile = [pname fname];
 end
 
 % Read the image file
 if ischar(imagefile)
-    fprintf('Reading %s...', imagefile);
-    im = imread(imagefile);
-    imsz = size(im);
-    totPix = imsz(1) * imsz(2);
-    fprintf('Done. Read %d pixels\n', totPix);   
-else   
-    im = imagefile;
-    imsz = size(im);
-    totPix = imsz(1) * imsz(2);
+fprintf('Reading %s...', imagefile);
+im = imread(imagefile);
+imsz = size(im);
+totPix = imsz(1) * imsz(2);
+fprintf('Done. Read %d pixels\n', totPix);
+else
+im = imagefile;
+imsz = size(im);
+totPix = imsz(1) * imsz(2);
 end
 
 im = im2single(im);
@@ -63,68 +63,68 @@ masks = false(size(im,1), size(im,2), k);
 
 % iterate through the colors
 for ik = 1:k
-  fprintf('Color %d of %d\n', ik, k);  
-  % c is a 1 or 3 element vector containing the current color
-  c = im_kc(ik,:);
+fprintf('Color %d of %d\n', ik, k);
+% c is a 1 or 3 element vector containing the current color
+c = im_kc(ik,:);
 
-  fprintf('Generating mask...\n');
-  % mask is a logical image that is true where im is the current
-  % color, and false where it is not.
-  mask = getMask(im, c);
+fprintf('Generating mask...\n');
+% mask is a logical image that is true where im is the current
+% color, and false where it is not.
+mask = getMask(im, c);
 %   extracted_mask = or(extracted_mask, mask);
-  masks(:,:,ik) = mask;
+masks(:,:,ik) = mask;
 
-  fprintf('Collecting region properties...\n');
-  % str_ik is a struct array with the field Area.
-  % There is one element in the array per connected-component
-  % in the image, and Area corresponds to its area, in pixels
-  str_ik = regionprops(mask, 'Area');
-  
-  fprintf('Collating statistics...\n');
-  % Black or white is expected to be background
-  % In this case, clump the areas into a single sum
-  if all(c == 0) || all(c == max(im_kc(:)))
-    sumArea = sum([str_ik.Area]);
-    str_ik = str_ik(1);
-    str_ik.Area = sumArea;
-  end
-  
-  % Add fields for group number, color and fraction to each struct
-  for is = 1:numel(str_ik)
-    str_ik(is).Color = c;
-    str_ik(is).idx = ik;
-    str_ik(is).Fraction = str_ik(is).Area / totPix;
-  end
+fprintf('Collecting region properties...\n');
+% str_ik is a struct array with the field Area.
+% There is one element in the array per connected-component
+% in the image, and Area corresponds to its area, in pixels
+str_ik = regionprops(mask, 'Area');
 
-  % Concatenate the result into the return value
-  if (numel(str_ik) > 0)
-    stat_str = cat(1, stat_str, str_ik);
-  end
-  
-  fprintf('Done.\n');
+fprintf('Collating statistics...\n');
+% Black or white is expected to be background
+% In this case, clump the areas into a single sum
+if all(c == 0) || all(c == max(im_kc(:)))
+sumArea = sum([str_ik.Area]);
+str_ik = str_ik(1);
+str_ik.Area = sumArea;
+end
+
+% Add fields for group number, color and fraction to each struct
+for is = 1:numel(str_ik)
+str_ik(is).Color = c;
+str_ik(is).idx = ik;
+str_ik(is).Fraction = str_ik(is).Area / totPix;
+end
+
+% Concatenate the result into the return value
+if (numel(str_ik) > 0)
+stat_str = cat(1, stat_str, str_ik);
+end
+
+fprintf('Done.\n');
 end
 
 % stat_str = cat(1, stat_str{:});
 
 % If we're given an output file
 if nargin > 1 && ~isempty(outputfile)
-  % open the file
-  f = fopen(outputfile, 'w');
-  % f is the file handle. f < 1 indicates an error of some kind
-  if f < 1
-    fprintf('Could not open file %s for writing\n', outputfile);
-  else
-    % Write the header row
-    fprintf(f, 'Group ID, Color, Pixel Count, Fraction of Image\n');
-    % Then write the data rows
-    for is = 1:numel(stat_str)
-      str_i = stat_str(is);
-      fprintf(f, '%d, %s, %d, %g\n', str_i.idx, hexColor(str_i.Color * 255), ...
-	str_i.Area, str_i.Fraction);
-    end
-    % close and flush the file. We're done.
-    fclose(f);
-  end  
+% open the file
+f = fopen(outputfile, 'w');
+% f is the file handle. f < 1 indicates an error of some kind
+if f < 1
+fprintf('Could not open file %s for writing\n', outputfile);
+else
+% Write the header row
+fprintf(f, 'Group ID, Color, Pixel Count, Fraction of Image\n');
+% Then write the data rows
+for is = 1:numel(stat_str)
+str_i = stat_str(is);
+fprintf(f, '%d, %s, %d, %g\n', str_i.idx, hexColor(str_i.Color * 255), ...
+        str_i.Area, str_i.Fraction);
+end
+% close and flush the file. We're done.
+fclose(f);
+end
 end
 
 
@@ -138,10 +138,10 @@ fprintf('Processing color %03d %03d %03d...', c(1) * 255, c(2) * 255, c(3)* 255)
 % Start out everywhere true.
 mask = true(size(im,1), size(im,2));
 for ic = 1:numel(c)
-  % Set the mask to false if the image's channel
-  % doesn't match the corresponding component of
-  % the color. This works for RGB or grayscale.
-  mask = and(mask, abs(im(:,:,ic) - c(ic)) < (16 / 256));
+% Set the mask to false if the image's channel
+% doesn't match the corresponding component of
+% the color. This works for RGB or grayscale.
+mask = and(mask, abs(im(:,:,ic) - c(ic)) < (16 / 256));
 end
 
 fprintf('done.\n');
@@ -159,7 +159,7 @@ function hex = hexColor(c)
 % be the same. We use < 3 rather than == 1 for
 % robustness. Plus, it looks like a heart. Kind of.
 if numel(c) < 3
-  repmat(c, [1 3]);
+repmat(c, [1 3]);
 end
 
 
