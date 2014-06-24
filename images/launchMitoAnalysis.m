b14ae6cf3a89222c9121eb0b0db8e0e2fb8ddadc
Larry Lindsey <llindsey@clm.utexas.edu>
2014-06-23 19:23:58 -0500

    launcMitoAnalysis: changes that maybe should have been commited already

--- images/launchMitoAnalysis.m
+++ images/launchMitoAnalysis.m
@@ -23,29 +23,30 @@ for ii = 1:numel(dd)
         mdir = dir([dirname '/*' extM]);
         
         if numel(adir) ~= numel(mdir)
-            error('Found %d annotation files and %d mito files',...
-                numel(adir), numel(mdir));
-        end
-        
-        afiles = strcat([dirname '/'], sort({adir.name}));
-        mfiles = strcat([dirname '/'], sort({mdir.name}));
-        ids = repmat({dd(ii).name}, size(afiles));
-        
-        allAFiles = cat(2, allAFiles, afiles);
-        allMFiles = cat(2, allMFiles, mfiles);
-        allIds = cat(2, allIds, ids);
-        
-        % check for unused files.
-        alldir = dir(dirname);
-        fsel = [alldir.isdir] == 0;
-        alldir = alldir(fsel);
-        alldirfiles = {alldir.name};
-        unusedfiles = setdiff(alldirfiles, cat(2, {adir.name}, {mdir.name}));
-        if numel(unusedfiles) > 0
-            fprintf('Did not process the following files for dir %s:\n',...
-                dirname);
-            fprintf('%s\n', unusedfiles);
-            fprintf('\n');
+            warning('Found %d annotation files and %d mito files for dir %s',...
+                numel(adir), numel(mdir), dirname);
+        else
+            
+            afiles = strcat([dirname '/'], sort({adir.name}));
+            mfiles = strcat([dirname '/'], sort({mdir.name}));
+            ids = repmat({dd(ii).name}, size(afiles));
+            
+            allAFiles = cat(2, allAFiles, afiles);
+            allMFiles = cat(2, allMFiles, mfiles);
+            allIds = cat(2, allIds, ids);
+            
+            % check for unused files.
+            alldir = dir(dirname);
+            fsel = [alldir.isdir] == 0;
+            alldir = alldir(fsel);
+            alldirfiles = {alldir.name};
+            unusedfiles = setdiff(alldirfiles, cat(2, {adir.name}, {mdir.name}));
+            if numel(unusedfiles) > 0
+                fprintf('Did not process the following files for dir %s:\n',...
+                    dirname);
+                fprintf('%s\n', unusedfiles);
+                fprintf('\n');
+            end
         end
     end
 end
