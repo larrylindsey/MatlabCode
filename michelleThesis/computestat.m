178834f84a9ce6925cebb5dbaabb40cbc230f3c2
Larry Lindsey <llindsey@clm.utexas.edu>
2014-07-05 18:58:03 -0500

    update

--- michelleThesis/computestat.m
+++ michelleThesis/computestat.m
@@ -1,16 +1,16 @@
-function [stat, e] = computestat(sname, cname, str, type, minArea, cal)
+function [stat, e] = computestat(sname, cname, str, type, minArea)
 % sname - stat name
 % cname - class name
 % str - the struct
 % type - 'a' or 'm' for annotation or mito
 
-% population
-pop = str.(cname).(type);
-pop(pop < minArea) = [];
+cal = .002 * .002;
 
-if nargin > 5
-    pop = pop * cal;
-end
+% population
+pop = str.(cname).(type) * cal;
+pop(pop < minArea * cal) = [];
+
+totarea = str.totpix * cal;
 
 if nargout > 1
     e = stderr(pop);
@@ -21,13 +21,13 @@ switch sname
         stat = numel(pop);
     case 'fraction'
         % non capillary pixels
-        noncappix = str.totpix - sum(str.capillary.a);
+        noncappix = totarea - sum(str.capillary.a * cal);
         stat = sum(pop) / noncappix;
     case 'avgsize'
         stat = mean(pop);
     case 'nnorm'
         % non capillary pixels
-        noncappix = str.totpix - sum(str.capillary.a);
+        noncappix = totarea - sum(str.capillary.a * cal);
         stat = numel(pop) / noncappix;
     otherwise
         error('don''t know how to compute stat %s', sname);
--- michelleThesis/generatePlots.m
+++ michelleThesis/generatePlots.m
@@ -4,14 +4,19 @@ if ~isfield(stat, 'group')
     error('Need group info. Did you run applyGroupType?');
 end
 
-global name_k strfields_f name_title_k strfield_title_f name_id_map ...
-    strfields_id_map f k cal_f doMito_k g minArea name_g
+% if ~isfield(stat, 'calibrated')
+%     error('Did you run calibrateStat?');
+% end
 
-name_k = {'terminal', 'glia', 'capillary', 'background'};
+global name_k strfields_f name_title_k strfield_title_f name_id_map ...
+    strfields_id_map f k doMito_k g minArea name_g fig_font_size ...
+    title_font_size
+
+name_k = {'terminal', 'glia', 'capillary', 'background', 'allMito'};
 strfields_f = {'nnorm', 'fraction', 'avgsize'};
 
-name_title_k = {'Terminal', 'Glia', 'Capillary', 'Background'};
-strfield_title_f = {'n Normalized by Area', 'Fraction', 'Average Size (um)'};
+name_title_k = {'Terminal', 'Glia', 'Capillary', 'Background', 'Mitochondria'};
+strfield_title_f = {'# / Area (um^{-2})', 'Fraction', 'Size (um^2)'};
 
 name_g = {'YV', 'YE', 'YEP', 'AV', 'AE', 'AEP'};
 
@@ -23,7 +28,7 @@ minArea = 256;
 name_id_map = struct;
 strfields_id_map = struct;
 
-doMito_k = [true, true, false, false, false];
+doMito_k = [true, true, false, false, false, false];
 
 for i_k = 1:k
     name_id_map.(name_k{i_k}) = i_k;
@@ -33,7 +38,10 @@ for i_f = 1:f
     strfields_id_map.(strfields_f{i_f}) = i_f;
 end
 
-cal_f = [1 1 .002 * .002];
+% cal_f = [1 1 .002 * .002];
+
+fig_font_size = 14;
+title_font_size = 16;
 
 g = 6;
 groups = [stat.group];
@@ -77,7 +85,7 @@ end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function generateGroupBarPlotHelper(cstat_g, i_k, i_f, type)
-global strfields_f cal_f minArea g name_k
+global strfields_f minArea g name_k
 figure;
 b = zeros(1, g);
 e = b;
@@ -86,7 +94,7 @@ cname = name_k{i_k};
 
 for i_g = 1:g
     [b(i_g), e(i_g)] = computestat(strfields_f{i_f}, cname,...
-        cstat_g(i_g), type, minArea, cal_f(i_f));
+        cstat_g(i_g), type, minArea);
 end
 bar(b);
 hold on;
@@ -117,11 +125,13 @@ end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function dataByAnimalPlotHelper(stat_g, i_k, i_f, type)
-global g strfields_f name_k cal_f minArea
+global g strfields_f name_k minArea
 
-mean_bar_width = .5;
+mean_bar_width = .25;
 
-figure;
+style = 'oxvds^*';
+
+hf = figure;
 hold on;
 cname = name_k{i_k};
 mbr_2 = mean_bar_width / 2; % half mean bar width
@@ -135,19 +145,24 @@ for i_g = 1:g
         sel = animal_idx_m == animal_idx_a(i_a);
         astat = computeSummary(stat(sel));
         stat_values(i_a) = computestat(strfields_f{i_f}, cname,...
-            astat, type, minArea, cal_f(i_f));
+            astat, type, minArea);
+        
+        plot(i_g, stat_values(i_a), style(i_a), 'LineWidth', 2);
     end
     e = stderr(stat_values);
     m = mean(stat_values);
-    plot(ones(size(stat_values)) * i_g, stat_values, 'o');
+
     plot(i_g + [-mbr_2, mbr_2], m * [1, 1], 'k', 'LineWidth', 2);
     plot(i_g * [1, 1], m + [-e, e], 'k', 'LineWidth', 2);    
 end
 makeItPretty(type, i_k, i_f);
+
+print(hf, '-dpng', sprintf('figure_%d_%d_%s.png', i_k, i_f, type));
+print(hf, '-depsc', sprintf('figure_%d_%d_%s.eps', i_k, i_f, type));
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function makeItPretty(type, i_k, i_f)
-global name_title_k strfield_title_f name_g
+global name_title_k strfield_title_f name_g fig_font_size title_font_size
 
 pfstring = '';
 switch type
@@ -158,11 +173,14 @@ switch type
 end
 
 title(sprintf(pfstring, name_title_k{i_k},...
-    strfield_title_f{i_f}), 'FontSize', 20);
+    strfield_title_f{i_f}), 'FontSize', title_font_size);
+
+ylabel(strfield_title_f{i_f}, 'FontSize', fig_font_size);
 
 set(gca, 'XTick', 1:6);
 set(gca, 'XTickLabel', name_g);
-set(gca, 'FontSize', 16);
+set(gca, 'FontSize', fig_font_size);
 
+xlim([0 7]);
 
 end
