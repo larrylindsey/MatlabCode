29ebe3136255d0791bbb4fb58ee081e9e35bf7f7
Larry Lindsey <llindsey@clm.utexas.edu>
2014-07-06 14:04:23 -0500

    computeSummary: no need for global vars

--- michelleThesis/computeSummary.m
+++ michelleThesis/computeSummary.m
@@ -1,16 +1,20 @@
 function stat = computeSummary(stat_m)
-global k name_k
+
 
 stat.afile = {stat_m.afile};
 stat.mfile = {stat_m.mfile};
 stat.animal = stat_m(1).animal;
 stat.totpix = sum([stat_m.totpix]);
 
-for i_k = 1:k
-    cname = name_k{i_k};
-    c_cat = [stat_m.(cname)];
-    stat.(cname).a = [c_cat.a];
-    stat.(cname).m = [c_cat.m];
+fields = fieldnames(stat_m);
+
+for i_f = 1:numel(fields)
+    cname = fields{i_f};
+    if isstruct([stat_m.(cname)])
+        c_cat = [stat_m.(cname)];
+        stat.(cname).a = [c_cat.a];
+        stat.(cname).m = [c_cat.m];
+    end
 end
 
 end
