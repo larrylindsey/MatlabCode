9e1f8f9331a0b3bcfe409c1c471d90ba18572e5f
Larry Lindsey <larry.f.lindsey@gmail.com>
2014-09-29 18:40:04 -0500

    enumerateContours: fix for empty Transform structs

--- reconstruct/enumerateContours.m
+++ reconstruct/enumerateContours.m
@@ -13,15 +13,17 @@ names = cell(1,p);
 parfor ip = 1:p
     names{ip} = {};
     for i_sec = 1:numel(secdocSplit{ip})
-        transforms = secdocSplit{ip}(i_sec).section.Transform;
-        for i_t = 1:numel(transforms)
-            tform = transforms(i_t);
-            for i_c = 1:numel(tform.Contour)
-                catName = tform.Contour(i_c).name;
-                if ~ischar(catName)
-                    catName = num2str(catName);
+        if isfield(secdocSplit{ip}(i_sec).section, 'Transform')
+            transforms = secdocSplit{ip}(i_sec).section.Transform;
+            for i_t = 1:numel(transforms)
+                tform = transforms(i_t);
+                for i_c = 1:numel(tform.Contour)
+                    catName = tform.Contour(i_c).name;
+                    if ~ischar(catName)
+                        catName = num2str(catName);
+                    end
+                    names{ip} = cat(1, names{ip}, catName);
                 end
-                names{ip} = cat(1, names{ip}, catName);
             end
         end
     end
